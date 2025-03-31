f4a89ce47100645297c7f2387c6ec5c46741d985
Nicolas Fisher <nicofisher2007@gmail.com>
2025-03-31 08:17:24 -0700

    Add Ramping to End Effector Motor
    Create TalonFXConfiguration with OpenLoopRampPeriod of 0.25s
    Remove Unused Imports

--- src/main/java/frc/robot/subsystems/EndEffector.java
+++ src/main/java/frc/robot/subsystems/EndEffector.java
@@ -1,26 +1,10 @@
 package frc.robot.subsystems;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.util.datalog.BooleanLogEntry;
-import edu.wpi.first.util.datalog.DataLog;
-import edu.wpi.first.util.datalog.DoubleLogEntry;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 
 public class EndEffector extends SubsystemBase {
     
@@ -31,6 +15,10 @@ public class EndEffector extends SubsystemBase {
     public EndEffector() {
         motor = new TalonFX(6);
         motorRequest = new VoltageOut(0);
+
+        motor.getConfigurator().apply(
+            new TalonFXConfiguration().OpenLoopRamps.withVoltageOpenLoopRampPeriod(0.25)
+        );
     }
 
     public double getMotorCurrent() {
